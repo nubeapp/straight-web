02edc4963539bf4bb2ec610f6c9e7467c7ec3860
nubeapp <git.nubeapp@gmail.com>
2023-12-07 19:45:41 +0100

    More styling and Dialog done. It needs to be filled with content

--- src/models/user.ts
+++ src/models/user.ts
@@ -2,6 +2,8 @@ import type { Organization } from "./organization";
 
 export interface User {
     id: string;
+    name: string;
+    surname: string;
     email: number;
     organization: Organization;
 }
--- src/pages/api/user-service.ts
+++ src/pages/api/user-service.ts
@@ -13,7 +13,6 @@ export async function getUsersByOrganizationId() {
 
         return users
     } catch (error) {
-        // Handle the error here
         console.error('Error in getUsersByOrganizationId:', error);
         throw error;
     }
